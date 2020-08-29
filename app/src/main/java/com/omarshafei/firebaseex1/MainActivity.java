693472eeccf0749eb46e2571fd316b18476333d4
Omar Shafei <omarshafhy.os@gmail.com>
2020-08-29 11:49:12 +0200

    Uploading some text to the Cloud Firestore

---
+++ app/src/main/java/com/omarshafei/firebaseex1/MainActivity.java
@@ -0,0 +1,60 @@
+package com.omarshafei.firebaseex1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private String TITLE_KEY = "title";
+    private String DESCRIPTION_KEY = "description";
+    private EditText titleEditText;
+    private EditText descriptionEditText;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titleEditText = findViewById(R.id.edit_text_title);
+        descriptionEditText = findViewById(R.id.edit_text_description);
+    }
+
+    public void saveNote(View view) {
+        String title = titleEditText.getText().toString();
+        String description = descriptionEditText.getText().toString();
+
+        Map<String, Object> note = new HashMap<>();
+        note.put(TITLE_KEY, title);
+        note.put(DESCRIPTION_KEY, description);
+
+        db.collection("Notebook").document("My First Note").set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Note Saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+}
